f26ed525ac37d186a6718c064bd14374ece9c74a
Pervushin Mikhail <postalfa1@gmail.com>
2017-11-28 00:21:31 +0300

    Base configuration

---
+++ src/main/resources/public/js/angular-app.js
@@ -0,0 +1,25 @@
+var app = angular.module("app", ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+            .state('default', {
+                url: '',
+                templateUrl: 'template/home.html',
+                data: {pageTitle: 'HomePage'},
+                controller: 'Hello'
+            });
+});
+
+app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+    }]);
+
+
+
+
+
+
