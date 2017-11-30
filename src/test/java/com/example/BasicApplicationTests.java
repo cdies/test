d0d005a5eafcb1b5033be7b6f04cdfe1c4714129
Pervushin Mikhail <postalfa1@gmail.com>
2017-11-30 12:30:51 +0300

    add simple tests

--- src/test/java/com/example/BasicApplicationTests.java
+++ src/test/java/com/example/BasicApplicationTests.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import com.example.model.Country;
+import com.example.repository.ClientRepository;
+import com.example.repository.CountryRepository;
+import com.example.repository.LoanRepository;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +15,36 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BasicApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Test
+    public void contextLoads() {
+
+        assertThat(clientRepository).isNotNull();
+        assertThat(loanRepository).isNotNull();
+        assertThat(countryRepository).isNotNull();
+    }
+
+    @Test
+    public void testDataBase() {
+        
+        Country testCountry = new Country();
+        testCountry.setCountryName("test");
+
+        countryRepository.save(testCountry);
+        
+        assertThat(countryRepository.findByCountryName("test").isPresent()).isTrue();
+        
+        countryRepository.delete(testCountry);
+        
+        assertThat(countryRepository.findByCountryName("test").isPresent()).isFalse();
+    }
 
 }
